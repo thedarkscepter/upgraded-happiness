d71b94ded54fcf7e6bf35c861dd773328300b10a
thedarkscepter <60280897+thedarkscepter@users.noreply.github.com>
2021-08-09 14:54:58 +0530

    Add files via upload

---
+++ C143-Project-main/App.js
@@ -0,0 +1,42 @@
+import React from "react";
+import HomeScreen from "./screens/Home";
+import { createAppContainer } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
+import { createMaterialTopTabNavigator } from "react-navigation-tabs";
+import { RFValue } from "react-native-responsive-fontsize";
+
+export default function App() {
+  return <AppContainer />;
+}
+
+const AppStackNavigator = createStackNavigator(
+  {
+    Home: {
+      screen: HomeScreen,
+      navigationOptions: {
+        headerShown: false
+      }
+    },
+    AppTopNav: {
+      screen: AppTopNavigation,
+      navigationOptions: {
+        headerBackTitle: null,
+        headerTintColor: "#fff",
+        headerTitle: "Recommended Movies",
+        headerStyle: {
+          backgroundColor: "#d500f9"
+        },
+        headerTitleStyle: {
+          color: "#fff",
+          fontWeight: "bold",
+          fontSize: RFValue(18)
+        }
+      }
+    }
+  },
+  {
+    initialRouteName: "Home"
+  }
+);
+
+const AppContainer = createAppContainer(AppStackNavigator);
